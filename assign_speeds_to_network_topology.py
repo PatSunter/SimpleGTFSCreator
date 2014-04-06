a34565bffc03f2155354cd6f0562a852285bfb8b
Pat Sunter <patdevelop@gmail.com>
2014-04-06 19:00:15 +1000

    Added better input processing to assign speeds to network script.

--- assign_speeds_to_network_topology.py
+++ assign_speeds_to_network_topology.py
@@ -3,6 +3,7 @@
 import os
 import re
 import sys
+from optparse import OptionParser
 
 import osgeo.ogr
 from osgeo import ogr
@@ -75,12 +76,28 @@ def assign_peak_speeds_bus_la_distance_based(route_segments_shp, mode_config):
     return
 
 if __name__ == "__main__":
-    mode = 'bus'
-    input_segments_fname = "/Users/pds_phd/Dropbox/PhD-TechnicalProjectWork/OSSTIP_BZE/Melbourne_GIS_NetworkDataWork/BZE_New_Network/bus-node-edges-12_dec/bus-edges-copy.shp"
 
-    mode_config = settings[mode]
+    parser = OptionParser()
+    parser.add_option('--segments', dest='inputsegments', help='Shapefile of line segments.')
+    parser.add_option('--service', dest='service', help="Should be 'train', 'tram' or 'bus'.")
+    (options, args) = parser.parse_args()
+
+    if options.inputsegments is None:
+        parser.print_help()
+        parser.error("No segments shapefile path given.")
+    if options.service is None:
+        parser.print_help()
+        parser.error("No service option requested. Should be one of %s" \
+            % (settings.keys()))
+    if options.service not in settings:
+        parser.print_help()
+        parser.error("Service option requested '%s' not in allowed set, of %s" \
+            % (options.service, settings.keys()))
+        
+    mode_config = settings[options.service]
+
     # Open in write-able mode, hence the 1 below.
-    route_segments_shp = osgeo.ogr.Open(input_segments_fname, 1)    
+    route_segments_shp = osgeo.ogr.Open(options.inputsegments, 1)    
     assign_free_speeds(route_segments_shp, mode_config)
     assign_peak_speeds_portion_free_speed(route_segments_shp, mode_config)
     # Close the shape files - includes making sure it writes
