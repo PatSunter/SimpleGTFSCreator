3151f60180b367c31e3a7fb6d690bf9782df0e66
Pat Sunter <patdevelop@gmail.com>
2014-07-18 01:01:42 +1000

    Added new function to calculate whether a stop is on motorway. Uses an algorithm that checks if at least one side is on the motorway for a specified distance down the line.

--- motorway_calcs.py
+++ motorway_calcs.py
@@ -3,12 +3,15 @@ from osgeo import ogr, osr
 
 import mode_timetable_info as m_t_info
 import topology_shapefile_data_model as tp_model
+import project_onto_line as lineargeom
 
 COMPARISON_EPSG = 28355
 # Put a _lot_ of leeway here, for stops that were added just off off-ramps
 # etc.
-SEG_END_NEAR_MOTORWAY_CHECK_DIST = 150
+STOP_NEAR_MOTORWAY_CHECK_DIST = 30
 #SEG_END_NEAR_MOTORWAY_CHECK_DIST = 50 # Value more appropriate for reg bus.
+ALONG_ROUTE_EITHER_SIDE_CHECK_DIST = 600
+ALONG_ROUTE_EITHER_SIDE_CHECK_NUM = 3
 
 def ensure_motorway_field_exists(route_segments_lyr):
     tp_model.ensure_field_exists(route_segments_lyr, tp_model.ON_MOTORWAY_FIELD, 
@@ -32,16 +35,65 @@ def create_motorways_buffer(mways_lyr, target_srs):
     mways_src_srs = mways_lyr.GetSpatialRef()
     mways_transform = osr.CoordinateTransformation(mways_src_srs, target_srs)
     all_mways_geom.Transform(mways_transform)
-    mways_buffer_geom = all_mways_geom.Buffer(SEG_END_NEAR_MOTORWAY_CHECK_DIST)
+    mways_buffer_geom = all_mways_geom.Buffer(STOP_NEAR_MOTORWAY_CHECK_DIST)
     print "...done creating motorways buffer."
     return mways_buffer_geom
+
+def stop_on_motorway(input_geom, route_geom, mways_buffer_geom,
+        route_geom_transform):
+    """Check if a stop is considered to be "on the motorway."
+    route_seg_transform must be a transform for the route segment to 
+    the same SRS as the mways_buffer_geom."""
+    on_motorway = False
+    current_loc = input_geom.GetPoint(0)
+    # Making a temp test geom because we need to reproject it.
+    mway_test_geom = ogr.Geometry(ogr.wkbPoint)
+    mway_test_geom.AddPoint(*current_loc)
+    mway_test_geom.Transform(route_geom_transform)
+    within = mways_buffer_geom.Contains(mway_test_geom)
+    if within:
+        # Check before, all are within. If that fails, check after.
+        inc = ALONG_ROUTE_EITHER_SIDE_CHECK_DIST / \
+            float(ALONG_ROUTE_EITHER_SIDE_CHECK_NUM)
+        all_within_before = True
+        for ii in range(1, ALONG_ROUTE_EITHER_SIDE_CHECK_NUM+1):
+            dist = ii * -inc
+            before_pt = lineargeom.move_dist_along_route(route_geom,
+                current_loc, dist)
+            before_geom = ogr.Geometry(ogr.wkbPoint)
+            before_geom.AddPoint(*before_pt)
+            before_geom.Transform(route_geom_transform)
+            within_before = mways_buffer_geom.Contains(before_geom)
+            before_geom.Destroy()    
+            if not within_before:
+                all_within_before = False
+                break
+        if all_within_before:
+            on_motorway = True    
+        else:
+           all_within_after = True
+           for ii in range(1, ALONG_ROUTE_EITHER_SIDE_CHECK_NUM+1):
+               dist = ii * inc
+               after_pt = lineargeom.move_dist_along_route(route_geom,
+                   current_loc, dist)
+               after_geom = ogr.Geometry(ogr.wkbPoint)
+               after_geom.AddPoint(*after_pt)
+               after_geom.Transform(route_geom_transform)
+               within_after = mways_buffer_geom.Contains(after_geom)
+               after_geom.Destroy()
+               if not within_after:
+                   all_within_after = False
+                   break
+           if all_within_after:
+               on_motorway = True
+    mway_test_geom.Destroy()
+    return on_motorway
 
 def segment_on_motorway(route_segment, mways_buffer_geom,
         route_seg_transform):
     """Check if a segment is considered to be "on the motorway."
     route_seg_transform must be a transform for the route segment to 
     the same SRS as the mways_buffer_geom."""
-    field_name = tp_model.ON_MOTORWAY_FIELD 
     seg_geom = route_segment.GetGeometryRef()
     seg_coords = seg_geom.GetPoints()
     assert len(seg_coords) == 2
