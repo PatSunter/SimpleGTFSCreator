88944c9922c1a3975fc4ee672d9c497aa94323f2
Pat Sunter <patdevelop@gmail.com>
2014-11-04 18:54:09 +1100

    added a simple func to read in a timedelta.

--- parser_utils.py
+++ parser_utils.py
@@ -8,3 +8,13 @@ def getlist(list_string):
     else:    
         parsed_list = list_string.split(',')
     return parsed_list
+
+def td_str_to_td(td_str):
+    """Reads in a timedelta in the format HH:MM"""
+    td_parts = td_str.split(':')
+    if len(td_parts) != 2:
+        raise ValueError
+    td_hrs = int(td_parts[0])
+    td_mins = int(td_parts[1])
+    td = timedelta(hours=td_hrs, minutes=td_mins)
+    return td
